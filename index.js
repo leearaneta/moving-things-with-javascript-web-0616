1704ef8c1680183a18d6080ed85b9b007251e2bd
leearaneta <lee.araneta@gmail.com>
2016-07-22 10:21:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+
+
+
+function moveDodgerRight() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`
+  }
+}
+
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
+
+function hopefullyThisWorks() {
+  document.addEventListener("keydown", function(event) {
+    if (event.which === 39) {
+      moveDodgerRight()
+    }
+  })
+}
+
+function hopefullyThisToo() {
+  document.addEventListener('keydown', function(e) {
+    if (e.which === 37) {
+      moveDodgerLeft()
+    }
+  })
+}
+
+
+$(document).ready(function() {
+  hopefullyThisWorks()
+  hopefullyThisToo()
+})
